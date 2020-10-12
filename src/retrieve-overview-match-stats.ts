2967b470fc2bde1063e04aec454ed3957acbdb7b
sebastientromp <sebastien.tromp@gmail.com>
2020-10-12 15:14:44 +0200

    preparation to add secondary stats when retrieving all reviews

--- src/retrieve-overview-match-stats.ts
+++ src/retrieve-overview-match-stats.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { gzipSync } from 'zlib';
 import { getConnection } from './db/rds';
 
@@ -21,47 +22,60 @@ export default async (event): Promise<any> => {
 		console.log('getting stats for user', userToken, targetReviewId, userInput?.userName);
 		// First need to add the userName column, then populate it with new process, then with hourly sync process
 		const query = `
-			SELECT * FROM replay_summary 
+			SELECT t1.*, statName, statValue FROM replay_summary t1
+			LEFT OUTER JOIN match_stats t2
+			ON t1.reviewId = t2.reviewId
 			WHERE (
-				uploaderToken = '${userInput?.uploaderToken || userToken}'
-				OR userId = '${userInput?.userId || userIdFromToken}'
-				OR userName = '${userInput?.userName || 'invalid_user_name'}'
+				t1.uploaderToken = '${userInput?.uploaderToken || userToken}'
+				OR t1.userId = '${userInput?.userId || userIdFromToken}'
+				OR t1.userName = '${userInput?.userName || 'invalid_user_name'}'
 			)
-			AND creationDate > '${startDate.toISOString()}'
-			ORDER BY creationDate DESC
+			AND t1.creationDate > '${startDate.toISOString()}'
+			ORDER BY t1.creationDate DESC
 		`;
 		console.log('prepared query', query);
 		const dbResults: readonly any[] = await mysql.query(query);
 		console.log('executed query', dbResults && dbResults.length, dbResults && dbResults.length > 0 && dbResults[0]);
 
-		const results: readonly GameStat[] =
+		// Merging results
+		const allReviewIds: readonly string[] =
 			!dbResults || (targetReviewId && !dbResults.some(result => result.reviewId === targetReviewId))
 				? []
-				: dbResults.map(result =>
-						Object.assign(new GameStat(), {
-							additionalResult: result.additionalResult,
-							coinPlay: result.coinPlay,
-							creationTimestamp: Date.parse(result.creationDate),
-							gameFormat: result.gameFormat,
-							gameMode: result.gameMode,
-							opponentCardId: result.opponentCardId,
-							opponentClass: result.opponentClass,
-							playerName: result.playerName,
-							playerCardId: result.playerCardId,
-							playerClass: result.playerClass,
-							playerRank: result.playerRank,
-							newPlayerRank: result.newPlayerRank,
-							playerDeckName: result.playerDeckName,
-							playerDecklist: result.playerDecklist,
-							buildNumber: result.buildNumber,
-							scenarioId: result.scenarioId,
-							opponentRank: result.opponentRank,
-							opponentName: result.opponentName,
-							result: result.result,
-							reviewId: result.reviewId,
-						} as GameStat),
-				  );
-		console.log('results filtered', results.length);
+				: dbResults.map(result => result.reviewId as string);
+		console.log('all review ids', allReviewIds);
+		const uniqueReviewIds: readonly string[] = [...new Set(allReviewIds)];
+		console.log('uniqueReviewIds', uniqueReviewIds);
+
+		const results: readonly GameStat[] = uniqueReviewIds.map(reviewId => buildReviewData(reviewId, dbResults));
+
+		// const results: readonly GameStat[] =
+		// 	!dbResults || (targetReviewId && !dbResults.some(result => result.reviewId === targetReviewId))
+		// 		? []
+		// 		: dbResults.map(result =>
+		// 				Object.assign(new GameStat(), {
+		// 					additionalResult: result.additionalResult,
+		// 					coinPlay: result.coinPlay,
+		// 					creationTimestamp: Date.parse(result.creationDate),
+		// 					gameFormat: result.gameFormat,
+		// 					gameMode: result.gameMode,
+		// 					opponentCardId: result.opponentCardId,
+		// 					opponentClass: result.opponentClass,
+		// 					playerName: result.playerName,
+		// 					playerCardId: result.playerCardId,
+		// 					playerClass: result.playerClass,
+		// 					playerRank: result.playerRank,
+		// 					newPlayerRank: result.newPlayerRank,
+		// 					playerDeckName: result.playerDeckName,
+		// 					playerDecklist: result.playerDecklist,
+		// 					buildNumber: result.buildNumber,
+		// 					scenarioId: result.scenarioId,
+		// 					opponentRank: result.opponentRank,
+		// 					opponentName: result.opponentName,
+		// 					result: result.result,
+		// 					reviewId: result.reviewId,
+		// 				} as GameStat),
+		// 		  );
+		console.log('results filtered', results.length, results && results.length > 0 && results[0]);
 
 		const stringResults = JSON.stringify({ results });
 		const gzippedResults = gzipSync(stringResults).toString('base64');
@@ -87,6 +101,35 @@ export default async (event): Promise<any> => {
 		console.log('sending back error reponse', response);
 		return response;
 	}
+};
+
+const buildReviewData = (reviewId: string, dbResults: readonly any[]): GameStat => {
+	const relevantReviews = dbResults.filter(review => review.reviewId === reviewId);
+	const mainReview = relevantReviews[0];
+	// console.log('building review data for', reviewId, mainReview, relevantReviews, dbResults);
+	return {
+		additionalResult: mainReview.additionalResult,
+		coinPlay: mainReview.coinPlay,
+		creationTimestamp: Date.parse(mainReview.creationDate),
+		gameFormat: mainReview.gameFormat,
+		gameMode: mainReview.gameMode,
+		opponentCardId: mainReview.opponentCardId,
+		opponentClass: mainReview.opponentClass,
+		playerName: mainReview.playerName,
+		playerCardId: mainReview.playerCardId,
+		playerClass: mainReview.playerClass,
+		playerRank: mainReview.playerRank,
+		newPlayerRank: mainReview.newPlayerRank,
+		playerDeckName: mainReview.playerDeckName,
+		playerDecklist: mainReview.playerDecklist,
+		buildNumber: mainReview.buildNumber,
+		scenarioId: mainReview.scenarioId,
+		opponentRank: mainReview.opponentRank,
+		opponentName: mainReview.opponentName,
+		result: mainReview.result,
+		reviewId: mainReview.reviewId,
+		// Fill in with other stats here
+	} as GameStat;
 };
 
 class GameStat {
