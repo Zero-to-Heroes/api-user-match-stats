51e7a32f8b4cfcb97238d889d4cc5cbc0cffb4c3
sebastientromp <sebastien.tromp@gmail.com>
2022-07-13 14:48:45 +0200

    add bgsHasPrizes info

--- src/retrieve-overview-match-stats.ts
+++ src/retrieve-overview-match-stats.ts
@@ -112,6 +112,7 @@ const buildReviewData = (review: any): GameStat => {
 		finalComp: review.finalComp,
 		levelAfterMatch: review.levelAfterMatch,
 		bgsPerfectGame: review.bgsPerfectGame === 1,
+		bgsHasPrizes: review.bgsHasPrizes,
 
 		mercHeroTimings:
 			!!review.mercHeroTimings?.length && review.mercHeroTimings.includes(',')
